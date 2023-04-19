77131e37910e17724e8154910923019c2ef34be2
kai63001 <supanat0245@gmail.com>
2023-04-19 22:09:24 +0700

    add icong

--- client/pages/_document.tsx
+++ client/pages/_document.tsx
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head >
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        {/* png favicon */}
+        <link rel="icon" href="/icon/favicon.png" type="image/png" />
+      </Head>
       <body>
         <Main />
         <NextScript />
