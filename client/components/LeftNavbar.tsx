cf2315c10abd7a4439fcdb6964c57f504a4167a6
kai63001 <supanat0245@gmail.com>
2023-04-19 23:23:26 +0700

    update prompt

--- client/components/LeftNavbar.tsx
+++ client/components/LeftNavbar.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image"
+import Image from "next/image";
 import { Rubik } from "next/font/google";
 
 const rubik = Rubik({
@@ -6,22 +6,32 @@ const rubik = Rubik({
   subsets: ["cyrillic"],
 });
 const LeftNevbar = () => {
-    return (
-        <nav className="h-screen fixed bg-primary w-14 md:w-80 px-3 md:px-5 py-5">
-          <div className="flex flex-col">
-            <div className="flex space-x-2 items-center justify-start">
-                <Image src="/icon/favicon.png" width={35} height={35} alt="Nouch Logo" />
-              <div className={`text-3xl ${rubik.className} hidden md:inline`}>Nouch</div>
-            </div>
-            <div className="flex flex-col mt-10">
-              <a href="/" className="flex items-center text-white bg-gradient-to-r from-cyan-500 to-blue-500 w-full rounded-md md:px-9 md:py-3 py-1 md:space-x-3 justify-center md:justify-start">
-                <Image src="/icon/cmd.png" width={25} height={25} alt="Prompt" />
-                <span className="hidden md:inline">Prompts</span>
-              </a>
-            </div>
+  return (
+    <nav className="h-screen fixed bg-primary w-14 md:w-80 px-3 md:px-5 py-5">
+      <div className="flex flex-col">
+        <div className="flex space-x-2 items-center justify-start">
+          <Image
+            src="/icon/favicon.png"
+            width={35}
+            height={35}
+            alt="Nouch Logo"
+          />
+          <div className={`text-3xl ${rubik.className} hidden md:inline`}>
+            Nouch
           </div>
-        </nav>
-    )
-}
+        </div>
+        <div className="flex flex-col mt-10">
+          <a
+            href="/"
+            className="flex items-center text-white bg-gradient-to-r from-cyan-500 to-blue-500 w-full rounded-md md:px-9 md:py-3 py-1 md:space-x-3 justify-center md:justify-start"
+          >
+            <Image src="/icon/cmd.png" width={25} height={25} alt="Prompt" />
+            <span className="hidden md:inline">Prompts</span>
+          </a>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
-export default LeftNevbar
+export default LeftNevbar;
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -3,9 +3,12 @@ import Layout from "@/components/Layout";
 export default function Home() {
   return (
     <Layout>
-      <div className="flex flex-col md:flex-row">
-        <div className="flex-auto w-full md:w-64">02</div>
-        <div className="flex-auto w-full md:w-32">03</div>
+      <div className="px-10 py-10">
+        <h1 className="text-4xl mb-5">Awesome ChatGPT or AI Prompts</h1>
+        <div className="flex flex-col md:flex-row md:space-x-10 ">
+          <div className="flex-auto w-full md:basis-1/2 bg-primary">02</div>
+          <div className="flex-auto w-full md:w-20 bg-primary">03</div>
+        </div>
       </div>
     </Layout>
   );
