c7c8f6c97342a647e3bf5d649a36514664ba4a5d
kai63001 <supanat0245@gmail.com>
2023-04-30 22:42:10 +0700

    remove not use func

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -2,7 +2,7 @@ import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
 import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
 import { SessionContextProvider, Session } from "@supabase/auth-helpers-react";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { store } from "@/store/store";
 import { Provider } from 'react-redux'
 
